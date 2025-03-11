843ad75a5fc5c306559b1268af5429b59ba0ea4d
Glycoene <qty2823650328@163.com>
2025-03-11 21:01:03 +0800

    Test Template v1

--- main.go
+++ main.go
@@ -6,13 +6,33 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+type Userinfo struct {
+	Username string `form:"username"`
+	Password string `form:"password"`
+}
+
 func main() {
 	r := gin.Default()
 	r.LoadHTMLGlob("./HTML/*")
-	r.GET("/", func(ctx *gin.Context) {
-		ctx.HTML(http.StatusOK, "index.html", gin.H{
-			"Test": "测试",
-		})
+	r.GET("/login", func(ctx *gin.Context) {
+		ctx.HTML(http.StatusOK, "login.html", nil)
+	})
+	r.POST("/login", func(ctx *gin.Context) {
+		var u Userinfo
+		err := ctx.ShouldBind(&u)
+		if err != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{
+				"ERROR": err.Error(),
+			})
+		} else if u.Password != "123" {
+			ctx.HTML(http.StatusOK, "login.html", gin.H{
+				"Status": "用户名或密码错误",
+			})
+		} else {
+			ctx.HTML(http.StatusOK, "blogpage.html", gin.H{
+				"Username": u.Username,
+			})
+		}
 	})
 	r.Run()
 }
